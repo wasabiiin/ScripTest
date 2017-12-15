f567d53aaaa3d53d27a124f71543815ca357ee00
norimi.akai <noriiim1@gmail.com>
2017-12-15 19:07:12 +0900

    課題と発展課題

---
+++ Assets/Test.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;   // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53;    // int型の変数mpを宣言し、53で初期化してください
+
+    // 攻撃用の関数
+    public void Attack(){
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage){
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    // mpを消費して魔法攻撃をするMagic関数を作ってください
+    public void Magic(){
+
+        if (mp >= 5){
+            // 魔法が使える場合
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        } else {
+            // 魔法が使えない場合    
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+public class Test : MonoBehaviour {
+    private void Start()
+    {
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        //攻撃用の関数を呼び出す
+        lastboss.Attack();
+
+        //防御用の関数を呼び出す
+        lastboss.Defence(3);
+    }
+
+    //Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+}
+
---
+++ Assets/Test2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+        private void Start()
+        {
+            int[] points = { 30, 20, 50, 10, 80 };
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                Debug.Log(points[i]);
+            }
+
+            for (int i = points.Length - 1; i >= 0; i--)
+            {
+                Debug.Log(points[i]);
+            }
+
+            //Bossクラスを生成
+            Boss lastboss = new Boss();
+
+            //Magic関数を１０回呼び出す
+            for (int i = 0; i < 11; i++)
+            {
+                lastboss.Magic();
+            }
+        }
+
+    
+}
